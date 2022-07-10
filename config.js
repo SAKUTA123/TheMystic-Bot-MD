66932c0c7a4f2268b8b77ba5f8b2343fc858a46a
Profile <91337596+SAKUTA123@users.noreply.github.com>
2022-07-11 00:17:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50575008120', '🤖 Mystic - Bot 🤖', true],
-  ['17722386341']
+  ['6287752078797', '👑 Mystic - Creador 👑', true],
+  ['6287760550924', '🤖 Mystic - Bot 🤖', true],
+  ['6287752078797']
 ] // Cambia los numero que quieras
 global.mods = [] 
 global.prems = [] 
